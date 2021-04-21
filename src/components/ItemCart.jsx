282153bc981d92ed7073a37f5980df0f96a55391
bernardopiane <bernardopiane@users.noreply.github.com>
2021-04-21 11:45:28 -0300

    Added qnt modifier, cartbar

--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { removeAt } from "../features/cart/cartSlice";
+import { removeAt, changeQnt } from "../features/cart/cartSlice";
 
-export default function ItemCart({ item, index }, key) {
+export default function ItemCart({ item: data, index }, key) {
   const dispatch = useDispatch();
   return (
     <div>
-      {item.name} - {item.value}
+      {data.item.name} - {data.item.value} / Cor : {data.color} / Size :
+      {data.size} / Quantity :
+      <input
+        type="number"
+        name="test"
+        value={data.qnt}
+        min="1"
+        id="test'"
+        style={{ margin: "0 0.5em", width: "5em" }}
+        onChange={
+          // dispatch(changeQnt(index, 1))
+          (value) => dispatch(changeQnt([index, value.target.value]))
+          // (value) => console.log(value.target.value, index)
+          // dispatch(changeQnt(index, value.target.value))
+        }
+      />
       <button onClick={() => dispatch(removeAt(index))}>Remove</button>
     </div>
   );
